ac3abc5320d9bec6ac23b8975e38fafae5264db4
t-cecilia-nguyen <cecilia.t.nguyen@gmail.com>
2023-12-19 11:56:34 -0500

    Updates for MainPage

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("form");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+
+function validateForm () {
+    
+}
