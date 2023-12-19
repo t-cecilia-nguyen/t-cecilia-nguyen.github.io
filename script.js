bccc5609a8483efe337998825a7dd4a6d5262a87
t-cecilia-nguyen <cecilia.t.nguyen@gmail.com>
2023-12-19 17:41:10 -0500

    Update form, add script

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-const form = document.getElementById("form");
-const name = document.getElementById("name");
-const email = document.getElementById("email");
-const message = document.getElementById("message");
-
-function validateForm () {
-    
-}
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+  }
